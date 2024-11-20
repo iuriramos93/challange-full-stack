86c50ed42055cec27a07d4177105147cd9ab8c61
Iuri Ramos <iuri.ramos41@gmail.com>
2024-11-20 13:19:51 -0300

    Challange finished

--- backend/src/modules/students/students-controller.js
+++ backend/src/modules/students/students-controller.js
@@ -2,27 +2,58 @@ const asyncHandler = require("express-async-handler");
 const { getAllStudents, addNewStudent, getStudentDetail, setStudentStatus, updateStudent } = require("./students-service");
 
 const handleGetAllStudents = asyncHandler(async (req, res) => {
-    //write your code
+    try {
+        const result = await getAllStudents();
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 
 });
 
 const handleAddStudent = asyncHandler(async (req, res) => {
-    //write your code
+    const { name, email, className, section, roll } = req.body;
+    const payload = { name, email, className, section, roll };
+    try {
+        const result = await addNewStudent(payload);
+        res.status(201).json(result);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 
 });
 
 const handleUpdateStudent = asyncHandler(async (req, res) => {
-    //write your code
+    const { id, name, email, className, section, roll } = req.body;
+    const payload = { id, name, email, className, section, roll };
+    try {
+        const result = await updateStudent(payload);
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 
 });
 
 const handleGetStudentDetail = asyncHandler(async (req, res) => {
-    //write your code
+    const { id } = req.params;
+    try {
+        const result = await getStudentDetail(id);
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 
 });
 
 const handleStudentStatus = asyncHandler(async (req, res) => {
-    //write your code
+    const { userId, reviewerId, status } = req.body;
+    try {
+        const result = await setStudentStatus({ userId, reviewerId, status });
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 
 });
 
